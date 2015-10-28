54003ee53c1aff56ed584438ae14814032b587bf
Betcheg <betcheg+github@gmail.com>
2015-10-28 16:15:21 +0100

    Ajout des interaction pour ajouter montage

--- app/src/main/java/betcheg/mlgphotomontage/LetTheMLGBegin.java
+++ app/src/main/java/betcheg/mlgphotomontage/LetTheMLGBegin.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 
 /**
@@ -17,21 +18,47 @@ import android.widget.ImageView;
 public class LetTheMLGBegin extends ActionBarActivity {
 
 String imageString;
+Button addMLG;
+Button annuler;
+HorizontalScrollView horizontal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         this.setContentView(R.layout.editor);
 
         Intent intent = getIntent();
         imageString = intent.getStringExtra("image");
 
+        addMLG= (Button) findViewById(R.id.b_addmontage);
+        annuler= (Button) findViewById(R.id.b_annuler);
+        horizontal= (HorizontalScrollView) findViewById(R.id.horizontal);
         ImageView imgView = (ImageView) findViewById(R.id.imgView);
-        // Set the Image in ImageView after decoding the String
         imgView.setImageBitmap(BitmapFactory.decodeFile(imageString));
+
+
+
+        addMLG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addMLG.setVisibility(View.GONE);
+                horizontal.setVisibility(View.VISIBLE);
+            }
+
+
+        });
+
+        annuler.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addMLG.setVisibility(View.VISIBLE);
+                horizontal.setVisibility(View.GONE);
+            }
+
+
+        });
 
     }
 }
